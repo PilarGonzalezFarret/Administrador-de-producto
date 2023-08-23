fdd3fe868a2eafd5b4b18fec1d07233c16e2c12d
Pilar González-Farret <pilar.gonzalezfarret@gmail.com>
2023-08-22 23:09:27 -0400

    comentarios
    Añado dos comentarios

--- client/src/App.js
+++ client/src/App.js
@@ -12,8 +12,8 @@ function App() {
     <div className="App">
         <h1>Product Manager</h1>
         <Routes> 
-          <Route path="/" element={<Main/>}/> 
-          <Route path="/products/:id/" element={<Details/>} />
+          <Route path="/" element={<Main/>}/>          {/* En amarillo está la ruta, parecido a como se ve en la parte de arriba del browser. Entre {} está el nombre del archivo view. */}
+          <Route path="/products/:id/" element={<Details/>} />    {/* Aquí la ruta termina en :id, pero en el componente va fuera de 'la ruta', con un +id. */}
           <Route path="/update/:id/" element={<Edit/>} />
           <Route path="/delete/:id/" element={<Edit/>} />
         </Routes> 
